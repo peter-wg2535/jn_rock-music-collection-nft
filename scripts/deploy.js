664117835695cbf4a39ef9e63b74929e39bd2815
technqvi <38780060+technqvi@users.noreply.github.com>
2022-05-17 22:37:14 +0700

    v2

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,13 +7,10 @@ async function main(){
     const rockMusicNFT=await RockMusicNFT.deploy("RockMusicNFT","1ROCK")
     console.log("Deployed contract successfully to "+rockMusicNFT.address)
 
-    //1_dream_theather_1.json   to ropsten by infura
-    //npx hardhat run scripts\deploy.js --network ropsten
-    // const newItemId=await rockMusicNFT.mint("https://ipfs.io/ipfs/Qma2ZZmHZJKePMsMuL4T6PJuusoeGnyVwQgAZ23hVKpChT")
 
-    //3_bon_jovi_1.json to polygon mumbai by alchemy
+    //0_bon_jovi_1.json to polygon mumbai by alchemy
     // npx hardhat run scripts\deploy.js --network mumbai
-    const newItemId=await rockMusicNFT.mint("https://ipfs.io/ipfs/QmPWevSK6ffujzjKe6o4H3muGgRH8ozXxSLYW2qkoi9g1j")
+    const newItemId=await rockMusicNFT.mint("https://ipfs.io/ipfs/QmXbqNTwB7pb4kxnpji6BmYYWGEBvX6DmuF5aHn4iUnjAf")
 
     console.log("NFT mints successfully : "+ newItemId )
 }
--- scripts/mint_alchemy.js
+++ scripts/mint_alchemy.js
@@ -7,9 +7,9 @@ async function main() {
   const provider = new ethers.providers.AlchemyProvider("maticmum", process.env.ALCHEMY_MUMBAI_API_KEY)
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
   const signer = wallet.connect(provider)
-
+  // SMF 
   const rockMusicNFT = new ethers.Contract(process.env.ALCHEMY_MUMBAI_CONTRACT_ADDRESS, abi, signer)
-  await rockMusicNFT.mint("https://ipfs.io/ipfs/QmUDGNKP3kJxhV86m63e31mvtzG7UCtgxqJADFSWdXgE2c")
+  await rockMusicNFT.mint("https://ipfs.io/ipfs/QmVKwU2miZzK6yEtz1tYukj2xRXDhhaAv9b8nBwVdUsTSq")
 
   console.log('NFT minted!')
 }
