5cc9daad2bc04aaa25f21328d1f091b1e546075f
technqvi <38780060+technqvi@users.noreply.github.com>
2022-06-20 16:18:48 +0700

    ok

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,6 +12,14 @@ async function main(){
     // npx hardhat run scripts\deploy.js --network mumbai
     const newItemId=await rockMusicNFT.mint("https://ipfs.io/ipfs/QmXbqNTwB7pb4kxnpji6BmYYWGEBvX6DmuF5aHn4iUnjAf")
 
+        //1_dream_theather_1.json   to ropsten by infura
+    //npx hardhat run scripts\deploy.js --network ropsten
+    // const newItemId=await rockMusicNFT.mint("https://ipfs.io/ipfs/Qma2ZZmHZJKePMsMuL4T6PJuusoeGnyVwQgAZ23hVKpChT")
+
+    //3_bon_jovi_1.json to polygon mumbai by alchemy
+    // npx hardhat run scripts\deploy.js --network mumbai
+    //const newItemId=await rockMusicNFT.mint("https://ipfs.io/ipfs/QmPWevSK6ffujzjKe6o4H3muGgRH8ozXxSLYW2qkoi9g1j")
+
     console.log("NFT mints successfully : "+ newItemId )
 }
 
